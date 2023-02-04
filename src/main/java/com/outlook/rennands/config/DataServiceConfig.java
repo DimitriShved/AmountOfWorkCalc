f4e5644ed55735556dc24315539a49f677aceb88
DimitriShved <dmitryshved@yahoo.com>
2023-02-05 00:30:36 +0300

    feat: add dataServiceConfig
    add jakarta.persistance dependency

---
+++ src/main/java/com/outlook/rennands/config/DataServiceConfig.java
@@ -0,0 +1,70 @@
+package com.outlook.rennands.config;
+
+import java.util.Properties;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.outlook.rennands.repos")
+@ComponentScan(basePackages = "com.outlook.rennands")
+public class DataServiceConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			EmbeddedDatabaseBuilder dBuilder = new EmbeddedDatabaseBuilder();
+			return dBuilder.setType(EmbeddedDatabaseType.H2).build();
+		} catch (Exception e) {
+			System.out.println("Embedded DataSource bean cannot be created");
+			return null;
+		}
+	}
+
+	@Bean
+	public Properties hibernateProperties() {
+		Properties hibernateProp = new Properties();
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		hibernateProp.put("hibernateProp.hbm2ddl.auto", "create-drop");
+		hibernateProp.put("hibernate.show_sql", true);
+		hibernateProp.put("hibernate.max_fetch_depth", 3);
+		hibernateProp.put("hibernate.jdbc.batch_size", 10);
+		hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+		return hibernateProp;
+
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory());
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		return new HibernateJpaVendorAdapter();
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("com.outlook.rennands.entities");
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setJpaProperties(hibernateProperties());
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		factoryBean.afterPropertiesSet();
+		return factoryBean.getNativeEntityManagerFactory();
+	}
+
+}
