89c1cb87214f89f1b86969744d728b6405b715cd
DimitriShved <dmitryshved@yahoo.com>
2023-03-13 13:26:27 +0300

    feat: add css

--- src/main/java/com/outlook/rennands/config/WebConfig.java
+++ src/main/java/com/outlook/rennands/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.ui.context.support.ResourceBundleThemeSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,10 +18,13 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import jakarta.annotation.PostConstruct;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.outlook.rennands.web" })
@@ -51,10 +55,12 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addViewController("/").setViewName("roadSections/list");
 	}
 
-	// i18n
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		// i18n
 		registry.addInterceptor(localeChangeInterceptor());
+		// css themes
+		registry.addInterceptor(themeChangeInterceptor());
 	}
 
 	// basic
@@ -85,6 +91,12 @@ public class WebConfig implements WebMvcConfigurer {
 		return interceptor;
 	}
 
+	// css themes
+	@Bean
+	ThemeChangeInterceptor themeChangeInterceptor() {
+		return new ThemeChangeInterceptor();
+	}
+
 	// i18n
 	@Bean
 	CookieLocaleResolver localeResolver() {
@@ -92,6 +104,22 @@ public class WebConfig implements WebMvcConfigurer {
 		cookieLocaleResolver.setDefaultLocale(new Locale("ru", "RU"));
 		cookieLocaleResolver.setCookieMaxAge(Duration.ofSeconds(3600));
 		return cookieLocaleResolver;
+	}
+
+	// css themes
+	@Bean
+	ResourceBundleThemeSource themeSource() {
+		return new ResourceBundleThemeSource();
+	}
+
+	// css themes
+	@Bean
+	CookieThemeResolver themeResolver() {
+		CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+		cookieThemeResolver.setDefaultThemeName("standart");
+		cookieThemeResolver.setCookieMaxAge(3600);
+		cookieThemeResolver.setCookieName("theme");
+		return cookieThemeResolver;
 	}
 
 }
